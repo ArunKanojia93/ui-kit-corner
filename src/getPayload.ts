6196e9e52c27c800d21178defb0f82ca482c4ab4
ArunKanojia93 <arun@pyzalabs.com>
2024-09-29 19:33:55 +0530

    feat: added nodemailer and resend to send verification email

--- src/getPayload.ts
+++ src/getPayload.ts
@@ -1,10 +1,21 @@
 import dotenv from "dotenv";
+import nodemailer from "nodemailer";
 import path from "path";
 import payload, { Payload } from "payload";
 import { InitOptions } from "payload/config";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
+});
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.resend.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "resend",
+    pass: process.env.RESEND_API_KEY,
+  },
 });
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -33,6 +44,11 @@ export const getPayloadClient = async ({ initOptions }: Args = {}): Promise<Payl
 
   if (!cached.promise) {
     cached.promise = payload.init({
+      email: {
+        transport: transporter,
+        fromName: "UIKitCorner",
+        fromAddress: "onboarding@resend.dev",
+      },
       secret: process.env.PAYLOAD_SECRET,
       local: !initOptions?.express,
       ...(initOptions || {}),
